8aba5a2853f9704f3df7bd444fba3a31462fc8ea
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-08-16 16:30:14 +0700

    Add project files.

---
+++ 0003Matrix/0003.cpp
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+int main() {
+    int m, n;
+    int matrix[100][100];
+    int sum[100][100];
+    scanf("%d %d", &m, &n);
+    for (int i = 0; i < m; i++) // loop แถว
+    {
+        for (int j = 0; j < n; i++) // loop หลัก
+        {
+            scanf("%d", matrix[i][j]);
+        }
+
+    }
+    for (int i = 0; i < m; i++) // loop แถว
+    {
+        for (int j = 0; j < n; i++) // loop หลัก
+        {
+            sum[j][i] += matrix[i][j];
+        }
+
+    }
+    printf("%d", sum);
+
+    return 0;
+}
