50adf0a8ee46d31edf6256d900df60369e195fd1
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-08-16 17:29:54 +0700

    Full

--- 0003Matrix/0003.cpp
+++ 0003Matrix/0003.cpp
@@ -1,26 +1,55 @@
 ﻿#include<stdio.h>
+
 int main() {
     int m, n;
-    int matrix[100][100];
-    int sum[100][100];
+    int matrix1[100][100];
+    int matrix2[100][100];
+    int sum[100][100] ;
     scanf("%d %d", &m, &n);
+
+    for (int i = 0; i < m; i++) // loop แถว set sum = 0
+    {
+        for (int j = 0; j < n; j++) // loop หลัก
+        {
+            sum[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < m; i++) // loop แถว matrix1
+    {
+        for (int j = 0; j < n; j++) // loop หลัก matrix1
+        {
+                scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++) // loop แถว matrix2
+    {
+        for (int j = 0; j < n; j++) // loop หลัก matrix2
+        {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+    
+
     for (int i = 0; i < m; i++) // loop แถว
     {
-        for (int j = 0; j < n; i++) // loop หลัก
+        for (int j = 0; j < n; j++) // loop หลัก
         {
-            scanf("%d", matrix[i][j]);
+            sum[i][j] += matrix1[i][j] + matrix2[i][j];
         }
-
     }
+
+        
     for (int i = 0; i < m; i++) // loop แถว
     {
-        for (int j = 0; j < n; i++) // loop หลัก
+        for (int j = 0; j < n; j++) // loop หลัก
         {
-            sum[j][i] += matrix[i][j];
+            printf("%d ", sum[i][j]);
         }
-
+        printf("\n");
     }
-    printf("%d", sum);
-
+    
+    
     return 0;
 }
